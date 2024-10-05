98899a18057dd273519f579ff5c387dff8b9c5dc
Seungjin Yang <slowmoyang@gmail.com>
2024-10-05 16:48:49 +0900

    use eza instead of exa
    see https://github.com/ogham/exa/issues/1243

--- config/fish/conf.d/alias.fish
+++ config/fish/conf.d/alias.fish
@@ -1,9 +1,9 @@
 alias rm="rm -i"
 alias c="clear"
 #
-alias ls="exa --group-directories-first"
-alias ll="exa --long --group-directories-first"
-alias la="exa --long -all --header --group-directories-first"
+alias ls="eza --group-directories-first"
+alias ll="eza --long --group-directories-first"
+alias la="eza --long -all --header --group-directories-first"
 alias l="ll"
 
 alias vim="nvim"
